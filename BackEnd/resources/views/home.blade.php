4b7be67e5fd60550f1c43308f30a35ba85327cb1
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-08-05 10:25:12 +0700

    fifth commit

--- BackEnd/resources/views/home.blade.php
+++ BackEnd/resources/views/home.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.index')
 
 @section('content')
-Home <br>
-{{ auth()->user()->name }}
 <div class="row">
     <div class="col-lg-6">
-        <div class="d-flex">
-            <a class="btn btn-sm btn-primary" href="{{ route('item.index') }}">Beli Barang</a>
-        </div>
 
         <div class="mt-2">
             <h4>Daftar Transaksi</h4>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">no</th>
-                        <th scope="col">barang</th>
-                        <th scope="col">quantity</th>
-                        <th scope="col">total</th>
-                        <th scope="col">status</th>
-                        <th scope="col">tanggal</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($transaksi as $item)
-                    <tr>
-                        <td scope="row">
-                            {{ $loop->iteration }}
-                        </td>
-                        <td>
-                            {{ $item->item->name }}
-                        </td>
-                        <td>
-                            {{ $item->quantity }}
-                        </td>
-                        <td>
-                            Rp. {{ $item->total }}
-                        </td>
-                        <td>
-                            {{ $item->status }}
-                        </td>
-                        <td>
-                            {{ $item->created_at->format('d-m-Y') }}
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="d-flex">
+                <a class="btn btn-sm btn-primary" href="{{ route('item.index') }}">Beli Barang</a>
+            </div>
+            <div class="mt-2">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">no</th>
+                            <th scope="col">barang</th>
+                            <th scope="col">quantity</th>
+                            <th scope="col">total</th>
+                            <th scope="col">status</th>
+                            <th scope="col">tanggal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($transaksi as $item)
+                        <tr>
+                            <td scope="row">
+                                {{ $loop->iteration }}
+                            </td>
+                            <td>
+                                {{ $item->item->name }}
+                            </td>
+                            <td>
+                                {{ $item->quantity }}
+                            </td>
+                            <td>
+                                Rp. {{ $item->total }}
+                            </td>
+                            <td>
+                                {{ $item->status }}
+                            </td>
+                            <td>
+                                {{ $item->created_at->format('d-m-Y') }}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
--- BackEnd/resources/views/item.blade.php
+++ BackEnd/resources/views/item.blade.php
@@ -1,42 +1,60 @@
 @extends('layouts.index')
 
 @section('content')
-Home <br>
-Hello {{ auth()->user()->name }}
-<br><br>
 
-Daftar Barang
-<table>
-    <thead>
-        <tr>
-            <th>no</th>
-            <th>barang</th>
-            <th>quantity</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($items as $item)
-        <tr>
-            <td>
-                {{ $loop->iteration }}
-            </td>
-            <td>
-                {{ $item->name }}
-            </td>
-            <td>
-                Rp. {{ $item->price }}
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="row mt-2">
+    <div class="col-lg-6">
+        <h4>Daftar Barang</h4>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">no</th>
+                    <th scope="col">Barang</th>
+                    <th scope="col">Item</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                <tr>
+                    <td scope="row">
+                        {{ $loop->iteration }}
+                    </td>
+                    <td>
+                        {{ $item->name }}
+                    </td>
+                    <td>
+                        Rp. {{ $item->price }}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="col-lg-6">
+        <div class="row">
+            <h4>Form Pembelian Barang</h4>
+            <form action="{{route('transaction.store')}}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="item_id" class="form-label">No Barang</label>
+                    <input type="number" name="item_id" class="form-control @error('item_id') is-invalid @enderror"
+                        value="{{ old('item_id') }}" placeholder="masukan no barang">
+                    @error('item_id')
+                    {{ $message }}
+                    @enderror
+                </div>
+                <div class="form-group mt-2">
+                    <label for="quantity" class="form-label">No Barang</label>
+                    <input type="number" name="quantity" class="form-control @error('quantity') is-invalid @enderror"
+                        value="{{ old('quantity') }}" placeholder="masukan no barang">
+                    @error('quantity')
+                    {{ $message }}
+                    @enderror
+                </div>
+                <button class="mt-2 btn btn-sm btn-primary" type="submit">Beli Sekarang</button> <br> <br>
+            </form>
+        </div>
+    </div>
+</div>
 
-<form action="{{route('transaction.store')}}" method="post">
-    @csrf
-    <br> <label>masukan no barang</label> <br>
-    <input type="number" name="item_id">
-    <br> <label>masukan jumlah</label> <br>
-    <input type="number" name="quantity"><br>
-    <button type="submit">Beli</button> <br> <br>
-</form>
 @endsection
