fabc1cbdbfee8737234bcb568a87545c3245a6e4
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-08-05 10:06:47 +0700

    third commit

--- BackEnd/resources/views/home.blade.php
+++ BackEnd/resources/views/home.blade.php
@@ -2,44 +2,48 @@
 
 @section('content')
 Home <br>
-Hello {{ auth()->user()->name }}
-<br><br>
-<a href="{{ route('item.index') }}">Beli Barang</a>
-<table>
-    <thead>
-        <tr>
-            <th>no</th>
-            <th>barang</th>
-            <th>quantity</th>
-            <th>total</th>
-            <th>status</th>
-            <th>tanggal</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($transaksi as $item)
-        <tr>
-            <td>
-                {{ $loop->iteration }}
-            </td>
-            <td>
-                {{ $item->item->name }}
-            </td>
-            <td>
-                {{ $item->quantity }}
-            </td>
-            <td>
-                Rp. {{ $item->total }}
-            </td>
-            <td>
-                {{ $item->status }}
-            </td>
-            <td>
-                {{ $item->created_at->format('d-m-Y') }}
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
+{{ auth()->user()->name }}
+<div class="row">
+    <div class="col-lg-6">
+        <div class="d-flex">
+            <a class="btn btn-sm btn-primary" href="{{ route('item.index') }}">Beli Barang</a>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">no</th>
+                    <th scope="col">barang</th>
+                    <th scope="col">quantity</th>
+                    <th scope="col">total</th>
+                    <th scope="col">status</th>
+                    <th scope="col">tanggal</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($transaksi as $item)
+                <tr>
+                    <td scope="row">
+                        {{ $loop->iteration }}
+                    </td>
+                    <td>
+                        {{ $item->item->name }}
+                    </td>
+                    <td>
+                        {{ $item->quantity }}
+                    </td>
+                    <td>
+                        Rp. {{ $item->total }}
+                    </td>
+                    <td>
+                        {{ $item->status }}
+                    </td>
+                    <td>
+                        {{ $item->created_at->format('d-m-Y') }}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
