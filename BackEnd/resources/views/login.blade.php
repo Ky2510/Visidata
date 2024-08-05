0a5862b68cf5d8a40cae8cd3d6d992743efb48bf
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-08-05 10:35:03 +0700

    login clear

--- BackEnd/resources/views/login.blade.php
+++ BackEnd/resources/views/login.blade.php
@@ -2,17 +2,28 @@
 
 @section('content')
 <h2>Halaman Login</h2>
-<form action="{{ route('login.store') }}" method="POST">
-    @csrf
-    <input type="text" name="email" placeholder="email">
-    @error('email')
-    <span style="text-danger">{{ $message }}</span>
-    @enderror
-    <input type="password" name="password" placeholder="password">
-    <button type="submit">
-        Simpan
-    </button>
-</form>
+<div class="row">
+    <div class="col-lg-6">
 
-<a href="{{ route('register') }}">Register</a>
+        <form action="{{ route('login.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" name="email" placeholder="email">
+            </div>
+            @error('email')
+            <span style="text-danger">{{ $message }}</span>
+            @enderror
+            <div class="form-group">
+                <label for="passoword" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="password">
+            </div>
+            <button class="btn btn-sm btn-primary mt-2" type="submit">
+                Masuk
+            </button>
+            <a href="{{ route('register') }}">Register</a>
+        </form>
+    </div>
+</div>
+
 @endsection
